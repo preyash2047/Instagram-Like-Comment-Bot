994c68b8fc2df8ecc99fd27c67a340d2a690b85d
Preyash Patel <preyash2047@GMAIL.com>
2021-04-25 14:01:07 +0530

    Update preyash

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ https://socialpros.co/instagram-daily-limits#Instagram%E2%80%99s_Daily_Limits_in
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
 # Local path to chrome driver
-chromedriver_path = "C:/Users/Annie/Desktop/chromedriver_win32/chromedriver.exe"
+chromedriver_path = "C:/chromedriver.exe"
 
 
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
@@ -19,10 +19,18 @@ Make adjustments below to tweak the bot to your liking
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
 # List of hashtags to go through
-hashtag_list = ['urbanandstreet', 'natureperfect']
+#hashtag_list = ['motivationalquotes', 'quoteoftheday', "quoteforlife", "lovequotes"]
+hashtag_list = ["motivationalquotesi", "motivationalquoteslover", "motivationalquotesaboutlife", "dailymotivationalquote", "motivationalquotesinc", "motivationalquotesforstudents", "motivationalquotesempire", "quotebisnis", "quotesviral", "quoteofthedayy", "quoteshidup", "quotesbisnis", "bestmotivationalquotes", "quotesonlove", "quotesforwomen", "quotesofday", "quotessad", "quotescreator", "motivationalwednesday", "quotesmotivation", "quotesbaperan", "quotessholawat", "quoteoftheday❤️", "quotespelajar", "quotesabouthim", "quoteshariini", "quotesherex", "quoteofinstagram", "quotesanimeindonesia", "quotesabouther"]
 
 # List of comments to be randonmly chosen from
-comments_list = ['Love this!', 'Nice shot :)', 'Amazing~', 'Looks great! :)', 'Beautiful']
+comments_list = ['Nice Quote. Checkout my @quote.inme for motivational quotes!',
+'Nice thought :) Check out mine also @quote.inme',
+'Amazing~ for more post like this check out @quote.inme',
+'Looks great! :) for amazing quotes check out @quote.inme',
+'Beautiful stay happy with my @quote.inme page',
+"follow @quote.inme to upleaft your soul, becouse only soul matters",
+"Hey guys, hope you are doing great check out my quote page for dailly motivational quotes"]
+
 # Number of posts to go through per hashtag
 number_of_posts = 100
 
@@ -32,8 +40,8 @@ chance_to_comment = 2
 
 # Time to wait in between processing instagram posts in seconds
 # Enter lower and upper limit in randint()
-wait_between_posts = randint(7, 16)
+wait_between_posts = randint(7, 13)
 
 # Time to wait in between liking a post and commenting on it in seconds
 # Enter lower and upper limit in randint()
-wait_to_comment = randint(10, 20)
+wait_to_comment = randint(7, 15)
